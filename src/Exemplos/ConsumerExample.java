70fd56ee60b018f49ba7808c4316ffccb52b781a
Reinaldo Junior <reifsjunior@gmail.com>
2024-05-06 19:20:30 -0300

    Criação do Projeto do Curos Java da DIO. Conteúdo Stream API.

---
+++ src/Exemplos/ConsumerExample.java
@@ -0,0 +1,27 @@
+package Exemplos;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+        // Usar o Consumer com expressão lambda para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        // Usar o Consumer para imprimir números pares no Stream
+        numeros.forEach(n -> {
+                    if (n % 2 == 0) {
+                        System.out.println(n);
+                    }
+                }
+        );
+    }
+}
